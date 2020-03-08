562f885500a55f49f990549df6f8fb30a92f4a4c
Myrian Katto <myriankatto@gmail.com>
2020-03-08 14:34:38 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+
+import FilterableProductTable from './components/FilterableProductTable';
+
+import data from './data.json';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronStore</h1>
+
+        <FilterableProductTable products={data} />
       </div>
     );
   }
